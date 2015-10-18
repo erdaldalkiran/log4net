ab9ddaa41c6df8c053f66079387e7d829cd1c075
ed <erdal@protonmail.com>
2015-10-19 01:58:17 +0300

    #000 ed adds some appender samples

---
+++ Appenders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using log4net;
+using log4net.Appender;
+
+namespace Appenders
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var logger = LogManager.GetLogger(typeof (Program));
+            
+            //var appender = new RollingFileAppender();
+            //appender.PreserveLogFileNameExtension
+
+            logger.Debug("Debug");
+            logger.Info("Info");
+            Console.WriteLine($"Now: {DateTime.Now}");
+            logger.Warn("Warn");
+            logger.Error("Error");
+            logger.Fatal("Fatal");
+
+
+            //For rolling file appender
+            while (!Console.KeyAvailable)
+            {
+                logger.Error(DateTime.Now.ToLongDateString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
