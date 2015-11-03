f185f6a777b1f588fe510eb8c65b6a8bacc80454
ed <erdal@protonmail.com>
2015-11-03 21:22:09 +0200

    #000 ed adds filters code snippets

---
+++ Log4Net.Filters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Log4Net.Filters
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            var logger = LogManager.GetLogger(typeof (Program));
+
+
+            logger.Debug("This is a Debug message");
+            logger.Info("This is a Info message");
+            logger.Warn("This is a Warn message");
+            logger.Error("This is a Error message");
+            logger.Fatal("This is a Fatal message");
+
+            Console.ReadKey();
+        }
+    }
+}
